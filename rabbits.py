2ca6ad05d1e22d9efe5792ecdebe3e66a48e854a
Andres <tuulemaa.dyndns.org>
2013-11-23 13:37:38 +0200

    rabbits over RPC

--- rabbits.py
+++ rabbits.py
@@ -1,10 +1,11 @@
 import sys, random, time
+import threading
 from threading import Thread, Lock, Event, Condition
 import urllib, urllib2, re
 
-game_over_event = Event()
-start_condition = Condition()
-game_over_event.clear()
+#game_over_event = Event()
+#start_condition = Condition()
+#game_over_event.clear()
 
 class Field(Thread):
 	_carrot_map = {}
@@ -12,13 +13,14 @@ class Field(Thread):
 	_lock = Lock()
 	_carrot_condition = Condition()
   
-	def __init__(self, size):
+	def __init__(self, size, game_over_event):
+		Thread.__init__(self)
 		self._size = size
 		#add carrots to map
 		self._carrot_map = [( 1 if random.randrange(10) > 7 else 0) for i in range(size*size)]
 		print self._carrot_map
 		print "Map size: %dx%d, carrots: %d" % (size, size, self.getCarrotCount())
-		Thread.__init__(self)
+		self.game_over_event = game_over_event
 		
 	def eatCarrot(self, pos):
 		index = pos[0] * self._size + pos[1]
@@ -45,7 +47,7 @@ class Field(Thread):
 		return sum(self._carrot_map)
 	
 	def isGameOver(self):
-		#print "isGameOver?", self.getCarrotCount() 
+		print "isGameOver?", self.getCarrotCount() 
 		return self.getCarrotCount() == 0
 	
 	
@@ -66,82 +68,107 @@ class Field(Thread):
 			self._carrot_condition.wait()
 		self._carrot_condition.release()
 		#print "game_over_event"
-		game_over_event.set()
-		#print "game_over_event set"
+		self.game_over_event.set()
+		print "game_over_event set"
 
 class Rabbit(Thread):
-	_field = None
-	_pos = ()
-	name = ""
-	carrots = 0
+	#_field = None
+	#_pos = ()
+	#name = ""
+	#carrots = 0
 
-	def __init__(self, name, field, pos):
-		print "Placing rabbit: %s to location [%d,%d]" % (name, pos[0], pos[1])
-		self._field = field;
-		self._pos = pos;
-		self.name = name;
-		self.moves = 0;
+	def __init__(self, name, field, pos, start_condition, game_over_event):
 		Thread.__init__(self)
-		
+		print "Placing rabbit: %s to location [%d,%d]" % (name, pos[0], pos[1])
+		self._field = field
+		self._pos = pos
+		self.name = name
+		self.moves = 0
+		self.start_condition = start_condition
+		self.game_over_event = game_over_event
+		self.carrots = 0
 
 	def run(self):
-		start_condition.acquire()
-		start_condition.wait()
-		start_condition.release()
+		self.start_condition.acquire()
+		self.start_condition.wait()
+		self.start_condition.release()
 
 		print "Rabbit: %s start @ %f" % (self.name, time.time())
-		while (not game_over_event.is_set()):
+		while (not self.game_over_event.is_set()):
 			(pos, carrot) = self._field.getNextCell(self._pos);
 			self.carrots += carrot
-			#print "Moving rabbit: %s from (%d, %d) to (%d,%d), found carrot? %d, carrots eaten: %d" % (self.name, self._pos[0], self._pos[1], pos[0], pos[1], carrot, self.carrots);
+			print "Moving rabbit: %s from (%d, %d) to (%d,%d), found carrot? %d, carrots eaten: %d" % (self.name, self._pos[0], self._pos[1], pos[0], pos[1], carrot, self.carrots);
 			if (carrot):
 				print "Rabbit %s found carrot @ (%d,%d)" % (self.name, pos[0], pos[1])
 			
 			self._pos = pos;
 			time.sleep(0.3);
 		
-		#echo "Rabbit {$this->name} is exiting field, carrots eaten: {$this->carrots}".PHP_EOL;
+		print "Rabbit "+self.name+" is exiting field, carrots eaten: "+str(self.carrots);
 		#flush();
 		
-class RabbitFun:
-	_map_size = 10;
-	_names = [];
-	_rabbits = [];
-	
-	def __init__(self, map_size, rabbit_count):
+class RabbitFun(Thread):
+	data = threading.local()
+	"""
+	_map_size = 10
+	_names = []
+	_rabbits = []
+	_status = "created"
+	field = None
+	"""
+	def __init__(self, name, map_size, rabbit_count):
 		self._map_size = map_size
 		print "Generating rabbit names...", 
 		self.generateNames(rabbit_count)
 		print "done"
 		print self._names
-	
-	def run(self) :
-		field = Field(self._map_size)
+		self.game_over_event = Event()
+		self.start_condition = Condition()
+		self.game_over_event.clear()
 		
-		field.start()
+		self.field = Field(self._map_size, self.game_over_event)
+		self._status = 'created'
+		
+
+		Thread.__init__(self)
+
+	def run(self) :
+		self._status = 'starting'
+		
+		self.field.start()
 		start = int(time.time())+1
+		self._rabbits = []
+		#print self, "self._rabbits 1 ", self._rabbits
 		for name in self._names:
-			self._rabbits.append(Rabbit(name, field, (random.randint(0,self._map_size-1), random.randint(0,self._map_size-1))))
+			self._rabbits.append(Rabbit(name, self.field, (random.randint(0,self._map_size-1), random.randint(0,self._map_size-1)), self.start_condition, self.game_over_event))
+		
+		print self, "self._rabbits 2 ", self._rabbits
 		
 		for rabbit in self._rabbits:
 			rabbit.start()
       
-		start_condition.acquire()
-		start_condition.notifyAll()
-		start_condition.release()
+		self.start_condition.acquire()
+		self.start_condition.notifyAll()
+		self.start_condition.release()
+		
+		self._status = 'running'
     
 		try:
-			while not game_over_event.is_set():
+			while not self.game_over_event.is_set():
 				time.sleep(.1)
 		except KeyboardInterrupt:
 			print "attempting to close threads"
-			game_over_event.set()
+			self.game_over_event.set()
 			print "threads successfully closed"
+			
+		self._status = 'stoping'
 
 		for rabbit in self._rabbits:
 			rabbit.join()
 		print "rabbits joined"
-		field.join()
+		self.field.join()
+		self._status = 'finished'
+		
 		winners = sorted(self._rabbits, key= lambda rabbit: rabbit.carrots, reverse=True);
 		winner = winners[0];
 		print "Winner is: %s with %d carrots!" % (winner.name, winner.carrots)
@@ -150,7 +177,34 @@ class RabbitFun:
 		for rabbit in winners:
 			print "#%d: %s ate %d carrots!" % (0, rabbit.name, rabbit.carrots)
 		
+	def getMapSize(self):
+		return self._map_size
+		
+	def getRabbitCount(self):
+		return len(self._rabbits)
+		
+	def getRabbits(self):
+		return self._names
 	
+			
+	def getStatus(self):
+		return self._status
+		
+	def getExtendedStatus(self):
+		if self._status == 'created':
+			return 'Press start to play the game'
+		elif self._status == 'running':
+			return 'Carrots remaining: %d' % self.getCarrotCount()
+		elif self._status == 'finished':
+			winners = sorted(self._rabbits, key= lambda rabbit: rabbit.carrots, reverse=True)
+			winner = winners[0]
+			return "Winner is: %s with %d carrots!" % (winner.name, winner.carrots)		
+		else:
+			return self._status
+								
+	def getCarrotCount(self):
+		return self.field.getCarrotCount()
+		
 	def generateNames(self, count):
     #http://www.voidspace.org.uk/python/articles/urllib2.shtml
       
@@ -168,6 +222,7 @@ class RabbitFun:
 		#print the_page
 		self._names = re.findall('<a class="firstname"[^>]+>([^<]+)</a>', the_page)
   
-
-fun = RabbitFun(int(sys.argv[1]), int(sys.argv[2]))
-fun.run()
+if __name__ == "__main__":
+	fun = RabbitFun(int(sys.argv[1]), int(sys.argv[2]))
+	fun.start()
+	fun.join()
---
+++ rabbits_server.py
@@ -0,0 +1,40 @@
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from rabbits import RabbitFun
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/runningrabbits_rpc',)
+
+# Create server
+server = SimpleXMLRPCServer(("localhost", 8000),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+games = {}
+
+class RabbitRPC:
+    def getRunningGames(self):
+        return map(lambda (k,v): {'name': k, 'map_size': v.getMapSize(), 'rabbits': v.getRabbits(), 'info':v.getExtendedStatus(), 'status':v.getStatus()}, games.iteritems())
+		
+    def createGame(self, name, map_size, rabbit_count):
+		if games.has_key(name):
+			raise Exception("Game "+ name+ "already exists!")
+		else:
+			games[name] = RabbitFun(name, int(map_size), int(rabbit_count))
+			print games[name]
+		return []
+		
+    def startGame(self, name):
+		print "start", name
+		if games.has_key(name) and games[name].getStatus() == 'created':
+			games[name].start()
+			return True
+		else:
+			raise Exception("Game "+ name+ "already started or does not exists!")
+		
+	
+server.register_instance(RabbitRPC())
+
+# Run the server's main loop
+server.serve_forever()
